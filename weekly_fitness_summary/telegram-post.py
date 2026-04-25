74c2aac58947090b5adcf23d681e4c619d3d5692
Jack <jlondon626@hotmail.co.uk>
2026-04-25 14:29:18 +0100

    update environment variables

--- weekly_fitness_summary/telegram-post.py
+++ weekly_fitness_summary/telegram-post.py
@@ -1,16 +1,21 @@
 import asyncio
 from datetime import datetime
 import telegram
+from dotenv import load_dotenv
+import os
 
-FITNESS_SUMMARY_BOT_TOKEN = "8587950405:AAEUGunScPbCuf59IV2AK5hXjnej5jYDqTM"
+load_dotenv()
+
+fitness_summary_bot_token = os.getenv("FITNESS_SUMMARY_BOT_TOKEN")
+telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
 async def main():
-    bot = telegram.Bot(FITNESS_SUMMARY_BOT_TOKEN)
+    bot = telegram.Bot(fitness_summary_bot_token)
     async with bot:
         # updates = (await bot.get_updates())[0]
         # print(updates)
         await bot.send_message(
-            chat_id=8402772488, 
+            chat_id=telegram_chat_id, 
             text="Okay, a slight delay when it goes cold, but it works! The weekly fitness summary will be sent every Sunday at 6pm.",
         )
 
