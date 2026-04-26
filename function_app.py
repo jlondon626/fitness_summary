0fb6604b257b86f445e3a3aacd83cd7bc163057f
Jack <jlondon626@hotmail.co.uk>
2026-04-26 22:10:03 +0100

    test time trigger

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ from weekly_fitness_summary.weekly_telegram_summary import main
 app = func.FunctionApp()
 
 @app.timer_trigger(
-    schedule="0 17 6 * * *",  # 06:17 UTC every day
+    schedule="0 */5 * * * *",  # 06:17 UTC every day
     arg_name="mytimer",
     run_on_startup=False,
     use_monitor=True,
